d754b7011222223fbfa869de5bdb1f8f060de4f3
Davi da Silva Fernades <davi.fernandes2012@gmail.com>
2018-05-17 15:51:51 -0300

    2

--- sql/sql/Program.cs
+++ sql/sql/Program.cs
@@ -16,6 +16,7 @@ namespace sql
 
 
             var Linst = new List<instancias>();
+            var sizeCamps = new instanciasSize();
             foreach (System.Data.DataRow item in x.Rows)
             {
                 var inst = new instancias
@@ -27,14 +28,32 @@ namespace sql
                     Version = item[4].ToString(),
                     IsLocal = item[5].ToString(),
                 };
+                sizeCamps.Name = inst.Name.Length > sizeCamps.Name ? inst.Name.Length : sizeCamps.Name;
+                sizeCamps.Server = inst.Server.Length > sizeCamps.Server ? inst.Server.Length : sizeCamps.Server;
+                sizeCamps.Instance = inst.Instance.Length > sizeCamps.Instance ? inst.Instance.Length : sizeCamps.Instance;
+                sizeCamps.IsClustered = inst.IsClustered.Length > sizeCamps.IsClustered ? inst.IsClustered.Length : sizeCamps.IsClustered;
+                sizeCamps.Version = inst.Version.Length > sizeCamps.Version ? inst.Version.Length : sizeCamps.Version;
+                sizeCamps.IsLocal = inst.IsLocal.Length > sizeCamps.IsLocal ? inst.IsLocal.Length : sizeCamps.IsLocal;
+
                 Linst.Add(inst);
             }
-            Console.WriteLine("---------------------------------------------------------");
-            Console.WriteLine("||Name||Server||Instance||IsClustered||Version||IsLocal||");
-
+            // Console.WriteLine("---------------------------------------------------------");
+            //Console.WriteLine("||Name||Server||Instance||IsClustered||Version||IsLocal||");
+            string d = $"||Name{new string(' ', sizeCamps.Name)}||Server{new string(' ', sizeCamps.Server)}||Instance{new string(' ', sizeCamps.Instance)}||IsClustered{new string(' ', sizeCamps.IsClustered)}||Version{new string(' ', sizeCamps.Version)}||IsLocal{new string(' ', sizeCamps.IsLocal)}||";
+            string s = $"||Name{new string(' ', sizeCamps.Name)}||Server{new string(' ', sizeCamps.Server)}";
+            Console.WriteLine(s);
             foreach (var item in Linst)
             {
-                Console.WriteLine(item.Server.Length);
+                item.Name = new string(' ', sizeCamps.Name - item.Name.ToString().Length) + item.Name.ToString();
+                item.Server = new string(' ', sizeCamps.Server - item.Server.ToString().Length) + item.Server.ToString();
+                item.Instance = new string(' ', sizeCamps.Instance - item.Instance.ToString().Length) + item.Instance.ToString();
+                item.IsClustered = new string(' ', sizeCamps.IsClustered - item.IsClustered.ToString().Length) + item.IsClustered.ToString();
+                item.Version = new string(' ', sizeCamps.Version - item.Version.ToString().Length) + item.Version.ToString();
+                item.IsLocal = new string(' ', sizeCamps.IsLocal - item.IsLocal.ToString().Length) + item.IsLocal.ToString();
+
+                //Console.WriteLine($"||{item.Name}||{item.Server}||{item.Instance}||{item.IsClustered}||{item.Version}||{item.IsLocal}||");
+                Console.WriteLine($"||{item.Name}||{item.Server}||");
+
             }
             Console.ReadLine();
         }
---
+++ sql/sql/model/instanciasSize.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sql.model
+{
+    public class instanciasSize
+    {
+        public int Name { get; set; }
+        public int Server { get; set; }
+        public int Instance { get; set; }
+        public int IsClustered { get; set; }
+        public int Version { get; set; }
+        public int IsLocal { get; set; }
+    }
+}
